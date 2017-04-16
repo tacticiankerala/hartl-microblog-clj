6402028a12af2425881112a164cef6df679d46aa
Sreenath N <tacticiankerala@gmail.com>
2017-04-16 13:32:13 +0530

    Add tools.nrepl lib for development

--- project.clj
+++ project.clj
@@ -19,9 +19,9 @@
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
-  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "hartl-microblog-clj.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.2"]]}
+                                        ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
+  :profiles {:dev {:aliases      {"run-dev" ["trampoline" "run" "-m" "hartl-microblog-clj.server/run-dev"]}
+                   :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                                  [io.pedestal/pedestal.service-tools "0.5.2"]]}
              :uberjar {:aot [hartl-microblog-clj.server]}}
   :main ^{:skip-aot true} hartl-microblog-clj.server)
-
